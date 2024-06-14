a352aedec51fc66efe5d9e07ed03527fff5c9797
ashiruhammed <hammedashiru89@gmail.com>
2024-06-14 22:38:35 +0100

    Improve patient layout with lazy loading for images

--- src/components/custom/sidebar.tsx
+++ src/components/custom/sidebar.tsx
@@ -7,6 +7,36 @@ import MessageIcon from '../icons/MessageIcon';
 import VideoCallIcon from '../icons/VideoCallIcon';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select';
+
+const routes = [
+  {
+    label: 'Dashboard',
+    url: '/patients',
+    icon: <DashboardIcon />,
+  },
+  {
+    label: 'Explore',
+    url: '/patients/explore',
+    icon: <ExploreIcon />,
+  },
+  {
+    label: 'Book Session',
+    url: '/patients/session',
+    icon: <VideoCallIcon />,
+  },
+  {
+    label: 'Message',
+    url: '/patients/message',
+    icon: <MessageIcon />,
+  },
+];
 
 export default function SideBar() {
   const pathname = usePathname();
@@ -19,34 +49,13 @@ export default function SideBar() {
 
   return (
     <div className='p-4'>
-      <div className='mt-8 mb-6 flex items-center gap-3'>
+      <div className='mt-8 mb-6  items-center gap-3 hidden md:flex'>
         <Image src='/logo.png' alt='logo' width={32} height={32} className='' />
         <h1 className='font-caveatSans text-2xl text-[#008037]'>Projectxx</h1>
       </div>
 
-      <ul className='space-y-3'>
-        {[
-          {
-            label: 'Dashboard',
-            url: '/patients',
-            icon: <DashboardIcon />,
-          },
-          {
-            label: 'Explore',
-            url: '/patients/explore',
-            icon: <ExploreIcon />,
-          },
-          {
-            label: 'Book Session',
-            url: '/patients/session',
-            icon: <VideoCallIcon />,
-          },
-          {
-            label: 'Message',
-            url: '/patients/message',
-            icon: <MessageIcon />,
-          },
-        ].map((item, index) => {
+      <ul className='space-y-3 hidden md:block'>
+        {routes.map((item, index) => {
           return (
             <li
               key={index}
@@ -59,6 +68,18 @@ export default function SideBar() {
           );
         })}
       </ul>
+      <Select>
+        <SelectTrigger className='p-0 outline-none border-none'>
+          <SelectValue>
+            <p className='p-4 rounded-full bg-[#E2E8F0]'>CN</p>
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent className='font-semibold text-[#334155]'>
+          <SelectItem value='light'>Light</SelectItem>
+          <SelectItem value='dark'>Dark</SelectItem>
+          <SelectItem value='system'>System</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
   );
 }
