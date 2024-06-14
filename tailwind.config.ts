95b67989a85dc1051e2abc868a7ab6df9711c5e1
herdeybayor <shereefdeen4real@gmail.com>
2024-06-14 11:15:35 +0100

    feat: add new form ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,9 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 import type { Config } from "tailwindcss";
 
+const colors = require("tailwindcss/colors");
+const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config = {
     darkMode: ["class"],
     content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
@@ -17,6 +20,9 @@ const config = {
         extend: {
             fontFamily: {
                 sans: ["var(--font-sans)", ...fontFamily.sans],
+            },
+            boxShadow: {
+                input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
             },
             colors: {
                 border: "hsl(var(--border))",
@@ -74,7 +80,16 @@ const config = {
             },
         },
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [require("tailwindcss-animate"), addVariablesForColors],
 } satisfies Config;
 
 export default config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+    addBase({
+        ":root": newVars,
+    });
+}
