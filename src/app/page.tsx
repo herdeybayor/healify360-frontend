234c1d4aa6f02d5bfeb433c46a2ef57536272046
ashiruhammed <hammedashiru89@gmail.com>
2024-06-17 23:55:49 +0100

    Update logo component and page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-import { ModeToggle } from "@/components/custom/mode-toggle";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
+import Logo from '@/components/custom/logo';
+import { ModeToggle } from '@/components/custom/mode-toggle';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-    return (
-        <main className="flex h-screen justify-center items-center">
-            <ModeToggle />
-            <Button asChild>
-                <Link href="/signup">Get Started</Link>
-            </Button>
-        </main>
-    );
+  return (
+    <main className=''>
+      <header>
+        <Logo />
+        <nav></nav>
+      </header>
+    </main>
+  );
 }
--- src/components/custom/logo.tsx
+++ src/components/custom/logo.tsx
@@ -1,22 +1,22 @@
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 export interface LogoProps {
-    href?: string;
-    className?: string;
+  href?: string;
+  className?: string;
 }
 
-function Logo({ href = "/", className }: LogoProps) {
-    return (
-        <Link href={href} className={cn("cursor-pointer", className)}>
-            <div className="items-center gap-3 flex">
-                <Image src="/logo.png" alt="logo" width={64} height={64} />
-                <h1 className="font-caveatSans text-2xl text-[#008037]">Healify360</h1>
-            </div>
-        </Link>
-    );
+function Logo({ href = '/', className }: LogoProps) {
+  return (
+    <Link href={href} className={cn('cursor-pointer', className)}>
+      <div className='items-center gap-3 flex'>
+        <Image src='/logo.png' alt='logo' width={64} height={64} />
+        <h1 className='font-caveatSans text-2xl text-[#008037]'>Healify360</h1>
+      </div>
+    </Link>
+  );
 }
 
 export default Logo;
