4987840e132e930be6e038173b3aee9e5210a370
herdeybayor <shereefdeen4real@gmail.com>
2024-06-17 22:53:11 +0100

    Add DoctorNavbar component and useUser hook

--- src/app/(guarded)/doctor/layout.tsx
+++ src/app/(guarded)/doctor/layout.tsx
@@ -1,3 +1,4 @@
+import DoctorNavbar from "@/components/custom/doctor-navbar";
 import DoctorSidebar from "@/components/custom/doctor-sidebar";
 import Logo from "@/components/custom/logo";
 import React from "react";
@@ -5,12 +6,12 @@ import React from "react";
 function DoctorsLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen w-full">
-            <div className="max-w-80 w-full bg-white h-full border-r border-gray-200 pt-6">
+            <div className="max-w-80 w-full bg-background h-full border-r border-gray-200 pt-6">
                 <Logo href="/doctor" />
                 <DoctorSidebar />
             </div>
-            <div className="flex-1 bg-red-200">
-                <nav className="bg-green-200">nav</nav>
+            <div className="flex-1">
+                <DoctorNavbar />
                 <main>{children}</main>
             </div>
         </div>
