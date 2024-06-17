becf93a5e0375e7c6bcd8066d9535769c5930032
ashiruhammed <hammedashiru89@gmail.com>
2024-06-18 00:08:33 +0100

    Update page header layout and navigation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 import Logo from '@/components/custom/logo';
-import { ModeToggle } from '@/components/custom/mode-toggle';
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className=''>
-      <header>
+      <header className='max-w-[1400px] items-center mx-auto py-5 flex justify-between'>
         <Logo />
-        <nav></nav>
+        <nav>
+          <ul className='items-center gap-8 lg:flex hidden'>
+            {['About Us', 'Services', 'How It Works', 'Contact Us'].map(
+              (item, index) => (
+                <li key={index} className='text-[#64748B]'>
+                  <Link href={`#`}>{item}</Link>
+                </li>
+              )
+            )}
+          </ul>
+        </nav>
+        <Link
+          href='/login'
+          className='text-sm font-[500] border py-2 px-4 rounded-md'>
+          Login
+        </Link>
       </header>
     </main>
   );
