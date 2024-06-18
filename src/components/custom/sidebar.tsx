868214ee774133e6500d150957d118f314a432da
herdeybayor <shereefdeen4real@gmail.com>
2024-06-18 12:04:15 +0100

    Refactor layout and sidebar components

--- src/components/custom/sidebar.tsx
+++ src/components/custom/sidebar.tsx
@@ -1,66 +1,58 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
-import DashboardIcon from '../icons/DashboardIcon';
-import ExploreIcon from '../icons/ExploreIcon';
-import MessageIcon from '../icons/MessageIcon';
-import VideoCallIcon from '../icons/VideoCallIcon';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import DashboardIcon from "../icons/DashboardIcon";
+import ExploreIcon from "../icons/ExploreIcon";
+import MessageIcon from "../icons/MessageIcon";
+import VideoCallIcon from "../icons/VideoCallIcon";
+import Logo from "./logo";
 
 const routes = [
-  {
-    label: 'Dashboard',
-    url: '/patients',
-    icon: <DashboardIcon />,
-  },
-  {
-    label: 'Explore',
-    url: '/patients/explore',
-    icon: <ExploreIcon />,
-  },
-  {
-    label: 'Book Session',
-    url: '/patients/session',
-    icon: <VideoCallIcon />,
-  },
-  {
-    label: 'Message',
-    url: '/patients/message',
-    icon: <MessageIcon />,
-  },
+    {
+        label: "Dashboard",
+        url: "/patients",
+        icon: <DashboardIcon />,
+    },
+    {
+        label: "Explore",
+        url: "/patients/explore",
+        icon: <ExploreIcon />,
+    },
+    {
+        label: "Book Session",
+        url: "/patients/session",
+        icon: <VideoCallIcon />,
+    },
+    {
+        label: "Message",
+        url: "/patients/message",
+        icon: <MessageIcon />,
+    },
 ];
 
 export default function SideBar() {
-  const pathname = usePathname();
-  const router = useRouter();
+    const pathname = usePathname();
 
-  const isActive = function (href: string) {
-    if (pathname === href) return true;
-    else return false;
-  };
+    const isActive = function (href: string) {
+        if (pathname === href) return true;
+        else return false;
+    };
 
-  return (
-    <div className='p-4'>
-      <div className='mt-8 mb-6  items-center gap-3 hidden md:flex'>
-        <Image src='/logo.png' alt='logo' width={32} height={32} className='' />
-        <h1 className='font-caveatSans text-2xl text-[#008037]'>Projectxx</h1>
-      </div>
+    return (
+        <div className="p-4">
+            <Logo href="/patients" className="hidden md:block" />
 
-      <ul className='space-y-3 hidden md:block'>
-        {routes.map((item, index) => {
-          return (
-            <li
-              key={index}
-              className={`flex items-center space-x-3 py-2 px-3 rounded-lg  ${
-                isActive(item.url) ? 'bg-[#00AC30] text-white' : 'text-black'
-              }`}>
-              {item.icon}
-              <Link href={item.url}>{item.label}</Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+            <ul className="space-y-3 hidden md:block">
+                {routes.map((item, index) => {
+                    return (
+                        <li key={index} className={`flex items-center space-x-3 py-2 px-3 rounded-lg  ${isActive(item.url) ? "bg-[#00AC30] text-white" : "text-black"}`}>
+                            {item.icon}
+                            <Link href={item.url}>{item.label}</Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
