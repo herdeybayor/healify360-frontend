25dde8b0df1e943bd9576baef15f00140e3f4ff4
herdeybayor <shereefdeen4real@gmail.com>
2024-06-18 23:22:11 +0100

    Add tabs component to SessionsPage

--- src/app/(guarded)/patient/session/page.tsx
+++ src/app/(guarded)/patient/session/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function SessionsPage() {
-    return <div>SessionsPage</div>;
+    return (
+        <div>
+            <h1 className="font-semibold text-2xl mb-4">Sessions</h1>
+            <Tabs defaultValue="all">
+                <TabsList className="grid w-full grid-cols-3">
+                    <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
+                    <TabsTrigger value="cancelled">Cancelled</TabsTrigger>
+                    <TabsTrigger value="all">All</TabsTrigger>
+                </TabsList>
+                <TabsContent value="upcoming">
+                    <div>upcoming</div>
+                </TabsContent>
+                <TabsContent value="cancelled">
+                    <div>cancelled</div>
+                </TabsContent>
+                <TabsContent value="all">
+                    <div>all</div>
+                </TabsContent>
+            </Tabs>
+        </div>
+    );
 }
 
 export default SessionsPage;
