a4db98208161ff6b9290a66b9769b72225148ffe
herdeybayor <shereefdeen4real@gmail.com>
2024-06-18 18:55:10 +0100

    Add patient profile create and get functions to http module

--- src/http/index.ts
+++ src/http/index.ts
@@ -4,3 +4,6 @@ export const AuthRegister = async (data: any) => $http.post(`/auth/register`, da
 export const AuthLogin = async (data: any) => $http.post(`/auth/login`, data).then((res) => res.data);
 
 export const UserGetSession = async () => $http.get(`/users/session`).then((res) => res.data);
+
+export const PatientProfileCreate = async (data: any) => $http.post(`/patients/profile/create`, data).then((res) => res.data);
+export const PatientProfileGet = async () => $http.get(`/patients/profile/get`).then((res) => res.data);
