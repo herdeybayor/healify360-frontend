e6de319da9d738e1f129686ba915b0c6fd4d24c6
ashiruhammed <hammedashiru89@gmail.com>
2024-06-18 10:24:05 +0100

    feat: Add carousel component to landing page
    This commit adds a carousel component to the landing page. The carousel allows for the display of multiple items in a slideshow format. It enhances the visual appeal and interactivity of the landing page. The carousel is implemented using the Embla Carousel library and includes autoplay functionality. This addition improves the overall user experience and engagement on the landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Logo from '@/components/custom/logo';
 import MenuIcon from '@/components/icons/MenuIcon';
 import { Button } from '@/components/ui/button';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { ArrowRight } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
+import Autoplay from 'embla-carousel-autoplay';
 
 const routes = ['About Us', 'Services', 'How It Works', 'Contact Us'];
 
@@ -127,6 +135,31 @@ export default function Home() {
               />
             </div>
           </div>
+        </div>
+        <div className='max-w-[1400px] mx-auto py-11 px-4'>
+          <Carousel
+            plugins={[
+              Autoplay({
+                delay: 2000,
+              }),
+            ]}
+            opts={{
+              loop: true,
+            }}>
+            <CarouselContent>
+              <CarouselItem>
+                <p>1ST</p>
+              </CarouselItem>
+              <CarouselItem>
+                <p>2ND</p>
+              </CarouselItem>
+              <CarouselItem>
+                <p>3RD</p>
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselNext />
+            <CarouselPrevious />
+          </Carousel>
         </div>
       </main>
     </div>
