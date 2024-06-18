846962b8458b035a5b727b8290d3518375272557
herdeybayor <shereefdeen4real@gmail.com>
2024-06-19 00:08:36 +0100

    Add new files and components

--- src/app/(guarded)/patient/message/page.tsx
+++ src/app/(guarded)/patient/message/page.tsx
@@ -1,14 +1,16 @@
+import { ChatLayout } from "@/components/custom/chat/chat-layout";
+import { cookies } from "next/headers";
 import React from "react";
 
 function MessagePage() {
+    const layout = cookies().get("react-resizable-panels:layout");
+    const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
     return (
-        <div>
-            <div className="flex items-center justify-between">
-                <h1 className="font-semibold text-2xl">Messages</h1>
-                <div className="flex items-center gap-4">
-                    <button className="btn btn-primary">New Message</button>
-                    <button className="btn btn-primary">New Group</button>
-                </div>
+        <div className="">
+            <h1 className="font-semibold text-2xl">Messages</h1>
+
+            <div className="z-10 border mt-8 rounded-lg w-full h-[80vh] text-sm lg:flex">
+                <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} />
             </div>
         </div>
     );
