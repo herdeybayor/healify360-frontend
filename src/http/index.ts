16bc000de80edcbf50769c83eecdc16d0905acf7
herdeybayor <shereefdeen4real@gmail.com>
2024-06-19 02:00:58 +0100

    Add appointment related API endpoints

--- src/http/index.ts
+++ src/http/index.ts
@@ -11,3 +11,8 @@ export const PatientProfileGet = async () => $http.get(`/patients/profile/get`).
 export const DoctorProfileCreate = async (data: any) => $http.post(`/doctors/profile/create`, data).then((res) => res.data);
 export const DoctorProfileGet = async () => $http.get(`/doctors/profile/get`).then((res) => res.data);
 export const DoctorFind = async () => $http.get(`/doctors/find`).then((res) => res.data);
+
+export const AppointmentBook = async (data: any) => $http.post(`/appointments/book`, data).then((res) => res.data);
+export const AppointmentGet = async (id: string) => $http.get(`/appointments/${id}`).then((res) => res.data);
+export const AppointmentGetAll = async () => $http.get(`/appointments/all`).then((res) => res.data);
+export const AppointmentUpdateStatus = async (id: string, data: any) => $http.patch(`/appointments/status/${id}`, data).then((res) => res.data);
