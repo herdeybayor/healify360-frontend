2a65cd16689e81404efb7058b89a65db27198c78
herdeybayor <shereefdeen4real@gmail.com>
2024-10-02 16:19:49 +0100

    Refactor JitsiMeeting component in session page

--- src/app/(guarded)/doctor/session/[sessionId]/page.tsx
+++ src/app/(guarded)/doctor/session/[sessionId]/page.tsx
@@ -25,7 +25,7 @@ function SessionPage() {
 
             {appointmentQuery && (
                 <JitsiMeeting
-                    domain={"healify360-web.cp.keenzchillz.properties"}
+                    // domain={"healify360-web.cp.keenzchillz.properties"}
                     roomName={`Meet with ${appointmentQuery.data.patient_profile_ref.full_name}`}
                     configOverwrite={{
                         startWithAudioMuted: false,
